6a38bb4a82d9d9a5b5ac7d84eba6ef4327bfe1fe
ShyCod <Nathaniel.jd@outlook.com>
2024-04-01 00:58:07 +0100

    Final Changes

--- assets/js/image-slider.js
+++ assets/js/image-slider.js
@@ -2,13 +2,10 @@ function autoScrollSlider() {
     const slider = document.querySelector('.slider');
     const scrollAmount = slider.scrollWidth / slider.children.length;
 
-    // Check if slider has reached the end
     if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
-        // Scroll back to the beginning
         slider.scrollLeft = 0;
     } else {
-        // Scroll to the next position
         slider.scrollLeft += scrollAmount;
     }
 }
-setInterval(autoScrollSlider, 6500);
+setInterval(autoScrollSlider, 4500);
